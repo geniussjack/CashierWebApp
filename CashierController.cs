d3e7900413d6fb00f90e9d76e846aa164386ba24
geniussjack <geniussjack@gmail.com>
2024-07-09 12:38:27 +0500

    Добавьте файлы проекта.

---
+++ CashierController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace CashierWebApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CashierController : ControllerBase
+    {
+        [HttpPost("print")]
+        public IActionResult PrintReceipt([FromBody] List<Product> product)
+        {
+
+            var receiptStrings = new List<string>();
+            foreach (var item in product)
+            {
+                receiptStrings.Add($"{item.Name} ({item.Amount} шт.)\nСтоимость..............................{item.Amount * item.Price}\n");
+            }
+            
+            return Ok(receiptStrings);
+        }
+    }
+    public class Product
+    {
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
